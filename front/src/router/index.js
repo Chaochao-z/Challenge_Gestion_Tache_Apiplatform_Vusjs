cd25dc272c6e8b357a385a6726a56d33b3c63bfe
Chaochao-z <chaochao.zhou@hotmail.com>
2023-05-29 16:01:27 +0200

    Front: Add BootStrap

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Homepage from '@/views/Accueil.vue'
+import Login from '../views/Login.vue'
+import NotFound from '../views/error/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,18 +12,14 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/seconde',
-      name: 'second',
-      component: Homepage
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path:'/:pathMatch(.*)*',component:NotFound
     }
+
   ]
 })
 
